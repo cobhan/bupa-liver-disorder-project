12b387e5cfcfc6200c76fa67691cd8a6995e10d1
Cóbhan Phillipson <cobhan.phillipson@gmail.com>
2018-07-19 00:05:42 +0100

    data import

---
+++ project.py
@@ -0,0 +1,17 @@
+# 7/18/18 Cóbhan Phillipson 
+# Liver Disorder GMIT Project
+
+# Import Python libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Open Bupa Liver Disorders Data Set
+f = open("data/bupa-data.csv")
+
+print(f)
+
+print(f.read())
+
+
+
