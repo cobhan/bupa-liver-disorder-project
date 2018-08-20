f0e0827824c2fce800b68afa2cb8a70c0c89c67b
Cóbhan Phillipson <cobhan.phillipson@gmail.com>
2018-08-20 22:20:25 +0100

    using panda to calculate mean, min, max

--- project.py
+++ project.py
@@ -6,12 +6,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# Open Bupa Liver Disorders Data Set
-f = open("data/bupa-data.csv")
+# Use panda to read Bupa Liver Disorders Data Set
+df = pd.read_csv('data/bupa-data.csv', names = ['mcv mean corpuscular volume', 'alkphos alkaline phosphotase', 'sgpt alamine aminotransferase', 'sgot aspartate aminotransferase', 'gammagt gamma-glutamyl transpeptidase', 'Daily Alcohol Drinks', 'Selector'])
 
-print(f)
+# Calculate Highest Value in Every Column
+print (df['mcv mean corpuscular volume'].max())
+print (df['alkphos alkaline phosphotase'].max())
+print (df['sgpt alamine aminotransferase'].max())
+print (df['sgot aspartate aminotransferase'].max())
+print (df['gammagt gamma-glutamyl transpeptidase'].max())
+print (df['Daily Alcohol Drinks'].max())
 
-print(f.read())
+# Calculate Mean Value of Every Column
+print (df['mcv mean corpuscular volume'].mean())
+print (df['alkphos alkaline phosphotase'].mean())
+print (df['sgpt alamine aminotransferase'].mean())
+print (df['sgot aspartate aminotransferase'].mean())
+print (df['gammagt gamma-glutamyl transpeptidase'].mean())
+print (df['Daily Alcohol Drinks'].mean())
+
+# Calculate Minimum Value of Each Column
+print (df['mcv mean corpuscular volume'].min())
+print (df['alkphos alkaline phosphotase'].min())
+print (df['sgpt alamine aminotransferase'].min())
+print (df['sgot aspartate aminotransferase'].min())
+print (df['gammagt gamma-glutamyl transpeptidase'].min())
+print (df['Daily Alcohol Drinks'].min())
 
 
 
